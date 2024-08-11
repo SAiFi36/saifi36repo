555b43cbd14324fb9a7189420a3d3b850fda0b90
SAiFi36 <120334331+SAiFi36@users.noreply.github.com>
2024-08-11 22:09:17 +0530

    Update home.php

--- home.php
+++ home.php
@@ -1,24 +1,19 @@
 <?php
-session_start();
-include 'config.php';
+session_start(); // Start the session here to check if the user is logged in
 
 // Redirect if session user is not set
 if (!isset($_SESSION['user'])) {
     header("Location: index.php");
     exit;
 }
-
-$user = htmlspecialchars($_SESSION['user']);
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
     <title>Home</title>
 </head>
 <body>
-    <h1>Hello <?php echo $user; ?></h1>
+    <h1>Hello <?php echo htmlspecialchars($_SESSION['user']); ?></h1>
     <p>Connection successful!</p>
-    <p><a href="logout.php">Logout</a></p>
 </body>
 </html>

8c8b79329f3ac329b23c854e61ddfecefef77b58
SAiFi36 <120334331+SAiFi36@users.noreply.github.com>
2024-08-11 22:07:24 +0530

    Update config.php

--- config.php
+++ config.php
@@ -7,9 +7,6 @@ $database = 'project';  // Replace with your MariaDB database name
 // Create a new MySQLi connection
 $conn = new mysqli($host, $username, $password, $database);
 
-// Start a session
-session_start();
-
 // Check if the connection was successful
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);

380f4ff02fa069bfd833acd3425db49c00bd4118
SAiFi36 <120334331+SAiFi36@users.noreply.github.com>
2024-08-11 22:20:03 +0530

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,7 +1,13 @@
 <?php
-session_start();
+session_start(); // Start the session
+
+// Unset all session variables
 session_unset();
+
+// Destroy the session
 session_destroy();
+
+// Redirect to the login page
 header("Location: index.php");
 exit;
 ?>

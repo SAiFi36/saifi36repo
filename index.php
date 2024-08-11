74b09b2c8b322582bbf29a8ab99732629d4087fd
SAiFi36 <120334331+SAiFi36@users.noreply.github.com>
2024-08-11 20:01:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,26 @@
-<HTML>
-        <HEAD>
-                <TITLE> SAiFi36 | Project </TITLE>
-        </HEAD>
-
-        <BODY>
-                <h1 align='center'> SAiFi36 | Project </h1>
-                <h1 align='center'>Jenkins + Docker Build&Push + Kubernetes Deployment</h1>
-
-                <hr>
-                <hr>
-                <form action='login.php' method='POST'>
-                <table align='center' border='1'>
-                        <tr>
-                                <td>Username </td>
-                                <td> <input type='text' name='uname'></input> </td>
-                        </tr>
-                        <tr>
-                                <td>Password </td>
-                                <td> <input  type='password' name='pass'></input></td>
-                        </tr>
-                        <tr>
-                                <td colspan=2 align='center'><input type='submit' name='submit' value='Submit'></input></td>
-                        </tr>
-                </table>
-                </form>
-        </BODY>
-</HTML>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>SAiFi36 | Project</title>
+</head>
+<body>
+    <h1 align='center'>SAiFi36 | Project</h1>
+    <h2 align='center'>Jenkins + Docker Build&Push + Kubernetes Deployment</h2>
+    <hr>
+    <form action='login.php' method='POST'>
+        <table align='center' border='1'>
+            <tr>
+                <td>Username</td>
+                <td><input type='text' name='uname' required></td>
+            </tr>
+            <tr>
+                <td>Password</td>
+                <td><input type='password' name='pass' required></td>
+            </tr>
+            <tr>
+                <td colspan=2 align='center'><input type='submit' name='submit' value='Submit'></td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
